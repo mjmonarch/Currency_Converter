9085fc6d751606cc4132b37405bc6ace1ef9cefe
Sergii Kamenskyi <serg@apple.com>
2022-01-08 09:15:50 +0200

    stage 2 completed!

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,1 +1,15 @@
-# write your code here!
+# ------------------------------STAGE 1-----------------------------------
+# print("Meet a conicoin!")
+
+# ------------------------------STAGE 2-----------------------------------
+import string
+
+my_template = string.Template("I have $number conicoins.\n"
+                              "$number conicoins cost $dollars dollars.\n"
+                              "I am rich! Yippee!")
+try:
+    conicoins = int(input())
+    dollars = conicoins * 100
+    print(my_template.substitute(number=conicoins, dollars=dollars))
+except ValueError:
+    print("You should enter number")
